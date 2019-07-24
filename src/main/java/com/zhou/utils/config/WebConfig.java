321a3820d949145191132effd50e76764f252191
eziozhou <m13718591023@163.com>
2019-07-24 18:30:53 +0800

    解决跨域和返回值乱码

---
+++ src/main/java/com/zhou/utils/config/WebConfig.java
@@ -0,0 +1,69 @@
+package com.zhou.utils.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * @author : Wenyu Zhou
+ * @version : v1.0
+ * @date : 2019/7/4
+ * description : 解决跨域，中英文乱码问题
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * 跨域支持
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600 * 24);
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter(){
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return converter;
+    }
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter messageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(getObjectMapper());
+        return converter;
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //解决中文乱码
+        converters.add(responseBodyConverter());
+        //解决 添加解决中文乱码后 上述配置之后，返回json数据直接报错 500：no convertter for return value of type
+        converters.add(messageConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+}
