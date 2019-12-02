fa9d034d610b5301d1307d8c1a4b4a9997c6af2f
w_p_xu@aliyun.com <w_p_xu@aliyun.com>
2019-12-02 17:20:31 +0800

    静态代理与动态代理

--- src/main/java/com/zhou/utils/utils/proxy/Proxy.java
+++ src/main/java/com/zhou/utils/utils/proxy/Proxy.java
@@ -18,8 +18,7 @@ public class Proxy {
         LogHandler logHandler=new LogHandler();
         UserManager userManager=(UserManager)logHandler.newProxyInstance(new UserManagerImpl());
         //UserManager userManager=new UserManagerImpl();
-        userManager.addUser("1111", "张三");
-
+        userManager.findUser("01");
     }
 
 }
